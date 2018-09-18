3841a6587447574c93f426508928075ebda5592b
Lea SANCHEZ <22092@PST8556.local>
2018-09-18 12:06:57 +0200

    add with carroussel

--- gallery.js
+++ gallery.js
@@ -1,7 +1,16 @@
+// var app = new Vue({
+//   el: '#app',
+//   components: {
+//     'carousel-3d': Carousel3d.Carousel3d,
+//     'slide': Carousel3d.Slide
+//   }
+// })
+
 var app = new Vue({
-  el: '#app',
+  el: '#gallery',
   components: {
     'carousel-3d': Carousel3d.Carousel3d,
     'slide': Carousel3d.Slide
   }
 })
+
